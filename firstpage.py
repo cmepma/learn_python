8f4d43f78badbc915b905c86d619b879b2dd8343
cmepma <160488723+cmepma@users.noreply.github.com>
2024-05-06 01:19:59 +0100

    Add files via upload

---
+++ firstpage.py
@@ -0,0 +1,36 @@
+#coding:utf-8
+
+
+"""les tupes de donnees 
+float int str bool
+"""
+NomClient = "nono"
+Agentcaisse = "leonel"
+prixHTT = 1200
+NomProduit = "chocolate"
+"""
+print("l'agent de caisse est:", Agentcaisse, ".")
+print("le prix de la commande HHTC est:" , prixHTT, ".")
+print("le nom du produit est:", NomProduit , ".")
+print("le nom du client est:" ,NomClient, ".")
+"""
+"""affection parallele
+a,b=5,8
+print(a)
+print (b)
+
+affectionsimple
+a1=450
+b2= 4582
+id(5) 
+
+
+"""
+
+
+
+
+
+i=+10
+for i in range (100):
+	print(str(i)) 
